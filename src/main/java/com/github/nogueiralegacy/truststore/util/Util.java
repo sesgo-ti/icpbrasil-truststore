4856eb1a75089335ffdecbf1343e14e3eee0a4a1
daniel nogueira <me@nogueiralegacy.com>
2025-09-22 18:11:26 -0300

    Util

---
+++ src/main/java/com/github/nogueiralegacy/truststore/util/Util.java
@@ -0,0 +1,23 @@
+package com.github.nogueiralegacy.truststore.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Slf4j
+@Component
+public class Util {
+
+    public InputStream getResource(String resourceName) throws NullPointerException {
+        if (resourceName == null) {
+           log.error("resourceName não pode ser null");
+        }
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(resourceName);
+        if (is == null) {
+            log.warn("Recurso {} não encontrado", resourceName);
+        }
+
+        return is;
+    }
+}
