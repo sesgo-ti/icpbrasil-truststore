37bf9eaef3224e6b5616789eeaaed5daf6a7de51
daniel nogueira <me@nogueiralegacy.com>
2025-10-02 16:27:13 -0300

    teste não é pesado

--- src/test/java/com/github/nogueiralegacy/truststore/util/DownloaderTest.java
+++ src/test/java/com/github/nogueiralegacy/truststore/util/DownloaderTest.java
@@ -2,16 +2,16 @@ package com.github.nogueiralegacy.truststore.util;
 
 import com.github.nogueiralegacy.truststore.config.TrustStoreConfig;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.yaml")
+@ActiveProfiles("test")
 public class DownloaderTest {
     @Autowired
     Downloader downloader;
@@ -19,14 +19,13 @@ public class DownloaderTest {
     @Autowired
     TrustStoreConfig trustStoreConfig;
 
-    @Disabled("Download de arquivo pesado")
     @SneakyThrows
     @Test
     void testDownlaodBytes() {
         byte[] bytes = downloader.downloadBytes(trustStoreConfig.getCertificateUrl());
 
         assertNotNull(bytes);
-        assertTrue(bytes.length > 0);
+        assertEquals(308843, bytes.length);
     }
 
     @SneakyThrows
