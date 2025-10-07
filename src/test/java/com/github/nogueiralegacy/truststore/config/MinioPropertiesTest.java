9147aec2ccea8ba6ff31fcb3661d55752e700cc4
daniel nogueira <me@nogueiralegacy.com>
2025-10-07 20:27:34 -0300

    Config to access Minio

---
+++ src/test/java/com/github/nogueiralegacy/truststore/config/MinioPropertiesTest.java
@@ -0,0 +1,28 @@
+package com.github.nogueiralegacy.truststore.config;
+
+import io.minio.MinioClient;
+import io.minio.messages.Bucket;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class MinioPropertiesTest {
+    @Autowired
+    MinioClient minioClient;
+
+    @SneakyThrows
+    @Test
+    void testConnectionWithMinIO() {
+        List<Bucket> buckets =  minioClient.listBuckets();
+
+        assertFalse(buckets.isEmpty());
+    }
+}
