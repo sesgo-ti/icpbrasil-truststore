c75b7852e388575ae1aef685613ac2a21f14273e
daniel nogueira <me@nogueiralegacy.com>
2025-10-16 15:14:26 -0300

    Teste de integração e carregamento do objeto CertificateData

---
+++ src/test/java/com/github/nogueiralegacy/truststore/config/VaultTest.java
@@ -0,0 +1,49 @@
+package com.github.nogueiralegacy.truststore.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.nogueiralegacy.truststore.model.CertificateData;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.vault.core.VaultTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class VaultTest {
+    @Autowired
+    private VaultTemplate vaultTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String DEFAULT_CERT_PATH = "certificates";
+
+
+    @Test
+    @SneakyThrows
+    void testConnectionWithVault() {
+        var kvOps = vaultTemplate.opsForVersionedKeyValue("kv");
+        var versionedSecret = kvOps.get(DEFAULT_CERT_PATH);
+        assertNotNull(versionedSecret, "Resposta do Vault não deve ser nula");
+    }
+
+    @Test
+    @SneakyThrows
+    void loadCertificateData() {
+        var kvOps = vaultTemplate.opsForVersionedKeyValue("kv");
+        var versionedSecret = kvOps.get(DEFAULT_CERT_PATH);
+        assertNotNull(versionedSecret, "Resposta do Vault não deve ser nula");
+
+        var dataMap = versionedSecret.getRequiredData();
+
+        for (var certEntry : dataMap.values()) {
+            CertificateData cert = objectMapper.readValue(certEntry.toString(), CertificateData.class);
+
+            assertNotNull(cert, "Certificado deve ser carregado");
+        }
+    }
+}
