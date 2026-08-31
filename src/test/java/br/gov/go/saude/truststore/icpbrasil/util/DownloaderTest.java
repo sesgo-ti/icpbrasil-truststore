b228b24b1dd5ac4295af0456c53a7531bc679ba6
nogueiralegacy <me@nogueiralegacy.com>
2026-08-31 10:11:38 -0300

    fix: move testes dependentes de rede externa para grupo integration excluído do build padrão

--- src/test/java/br/gov/go/saude/truststore/icpbrasil/util/DownloaderTest.java
+++ src/test/java/br/gov/go/saude/truststore/icpbrasil/util/DownloaderTest.java
@@ -3,12 +3,20 @@ package br.gov.go.saude.truststore.icpbrasil.util;
 import br.gov.go.saude.truststore.icpbrasil.config.TrustStoreConfig;
 import br.gov.go.saude.truststore.icpbrasil.http.Downloader;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Teste de integração: depende de conectividade real com o site do ITI
+ * (acraiz.icpbrasil.gov.br). Excluído da execução padrão do build; para
+ * executá-lo use o perfil {@code integration-tests}:
+ * {@code ./mvnw verify -Pintegration-tests}.
+ */
+@Tag("integration")
 @SpringBootTest
 class DownloaderTest {
     @Autowired
